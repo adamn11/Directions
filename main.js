e28bea8ad0a7768df370e33c5bfea4e17ee989bc
adamn11 <adamnguyen73@gmail.com>
2018-11-10 14:57:09 -0600

    Added multiple context menus

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
 chrome.runtime.onInstalled.addListener(function() {
     var context = "selection";
-    var title = "Search for Directions...";
-    var id = chrome.contextMenus.create({
-        "title": title, 
+
+    var point = chrome.contextMenus.create({
+        "title": "Search...", 
         "contexts": [context],
-        "id": "context" + context
+        "id": "point"
     });  
-  });
+
+    var direction = chrome.contextMenus.create({
+        "title": "Search Directions",
+        "contexts": [context],
+        "id": "direction"
+    })
+});
   
 chrome.contextMenus.onClicked.addListener(onClickHandler);
   
 function onClickHandler(info, tab) {
     var sText = info.selectionText;
-    var url = "https://www.google.com/maps/search/?api=1&query=" + encodeURIComponent(sText);  
+
+    if (info.menuItemId == "direction") {
+        console.log("direction");
+    } else if (info.menuItemId == "point") {
+        var url = "https://www.google.com/maps/search/?api=1&query=" + encodeURIComponent(sText);  
+    }
+
     window.open(url, '_blank');
 };
